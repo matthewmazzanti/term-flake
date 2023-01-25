fa45d05b48d839d63ba327d6a02aa7a25b8c7710
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2023-01-24 21:04:37 -0500

    Update nvim config

--- nvim/config/lspconfig/config.lua
+++ nvim/config/lspconfig/config.lua
@@ -43,19 +43,16 @@ local function setup(plugins)
         settings = {
             Lua = {
                 runtime = {
-                    -- Tell the language server which version of Lua you"re using
-                    -- (most likely LuaJIT in the case of Neovim)
-                    version = "LuaJIT",
+                    version = 'LuaJIT',
+                    path = runtime_path,
                 },
                 diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = {"vim"},
+                    globals = { 'vim' },
                 },
                 workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
+                    library = vim.api.nvim_get_runtime_file('', true),
+                    checkThirdParty = false,
                 },
-                -- Do not send telemetry data containing a randomized but unique identifier
                 telemetry = {
                     enable = false,
                 },
